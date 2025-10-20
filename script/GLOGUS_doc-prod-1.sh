a5bcb4e82035362932bdf32b6cfaebaffff7a200
Luc Lavoie <luc.lavoie@usherbrooke.ca>
2025-10-20 14:01:15 -0400

    Version préparatoire

---
+++ script/GLOGUS_doc-prod-1.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+# MCED_doc-prod-1.sh $1 $2
+
+document="${1:-MPS.adoc}"
+organisation="${2:-Metis}"
+echo "Production du document $document pour l’organisation $organisation"
+
+loc_outil=$rep_racine/CoLOED/Asciidoc/script
+loc_depot=$rep_racine/CoFELI/GLOGUS
+loc_source=$loc_depot/doc
+
+$loc_outil/AsciiDoc_gen.sh -d $loc_depot -o "${organisation}" -t -p -s $loc_source/"${document}"
